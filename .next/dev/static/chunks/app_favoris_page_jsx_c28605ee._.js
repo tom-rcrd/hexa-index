e96679164fceb038184732ef4c838a8f1e082eef
Tom <contact@hexa-index.fr>
2026-01-13 21:14:23 +0100

    pdate favoris

--- .next/dev/static/chunks/app_favoris_page_jsx_c28605ee._.js
+++ .next/dev/static/chunks/app_favoris_page_jsx_c28605ee._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_4f8afd7e._.js",
-  "static/chunks/node_modules_c3e3ca0e._.js"
+  "static/chunks/_0d7d32f3._.js",
+  "static/chunks/node_modules_af620b42._.js"
 ],
     source: "dynamic"
 });
